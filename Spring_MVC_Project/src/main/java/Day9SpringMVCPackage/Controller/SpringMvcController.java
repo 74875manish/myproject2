06cacd21a1db4cc29d4840cc0f3421eb3e806f5a
74875manish <128015030+74875manish@users.noreply.github.com>
2023-12-27 08:48:44 +0530

    Add files via upload

---
+++ Spring_MVC_Project/src/main/java/Day9SpringMVCPackage/Controller/SpringMvcController.java
@@ -0,0 +1,14 @@
+package Day9SpringMVCPackage.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller //Marks this class as a Controller
+public class SpringMvcController {
+	//Defining a request handling method
+	@RequestMapping("/showIndex")
+	public String getIndexPage() {
+		return "index";//Returns view name
+	}
+
+}
+
+
